caabd93898b71cc6fff44f792216866691db09b6
Bart <103577999+BartSchool@users.noreply.github.com>
2023-03-08 11:57:48 +0100

    changed model

--- CarbonCutters/CarbonCutters/Models/TripSegmentModel.cs
+++ CarbonCutters/CarbonCutters/Models/TripSegmentModel.cs
@@ -1,8 +1,16 @@
-﻿namespace CarbonCuttersView.Models
+﻿using CarbonCuttersCore;
+
+namespace CarbonCuttersView.Models
 {
     public class TripSegmentModel
     {
         public int distance { get; set; }
         public DateTime date { get; set; }
+        public Vehicle vehicle { get; set; }
+
+        public TripSegmentModel()
+        {
+
+        }
     }
 }
