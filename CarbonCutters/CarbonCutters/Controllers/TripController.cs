7faf80b3cb2e298c1a100a6347f8de7adf436306
Bart <103577999+BartSchool@users.noreply.github.com>
2023-03-27 15:10:52 +0200

    started with frontend rides

---
+++ CarbonCutters/CarbonCutters/Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarbonCuttersView.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarbonCutters/CarbonCutters/Models/TripModel.cs
@@ -0,0 +1,15 @@
+﻿using CarbonCuttersCore;
+using CarbonCuttersCore.Interface;
+using CarbonCuttersMockData;
+
+namespace CarbonCuttersView.Models
+{
+    public class TripModel
+    {
+        IUserCollection userCollection;
+        public TripModel()
+        {
+            userCollection = new MockUsers(30, 100);
+        }
+    }
+}
