7d9772a40d24d37305d7bf204596261d5ec114c0
Rick S <290278@student.fontys.nl>
2023-04-03 12:24:35 +0200

    fix

--- CarbonCutters/CarbonCutters/Controllers/LoginController.cs
+++ CarbonCutters/CarbonCutters/Controllers/LoginController.cs
@@ -6,7 +6,7 @@ namespace CarbonCuttersView.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(); // pas dit aan
         }
     }
 }
