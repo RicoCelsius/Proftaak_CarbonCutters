1aa99879888132201d055c69cbbf8da7127ecaa1
Rico <Rico.aarntzen@gmail.com>
2023-04-03 02:58:03 -0700

    deleted vs files and removed head

--- CarbonCutters/CarbonCutters/Controllers/LeaderboardController.cs
+++ CarbonCutters/CarbonCutters/Controllers/LeaderboardController.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CarbonCuttersView.Controllers
 {
     public class LeaderboardController : Controller
     {
-
+<<<<<<< HEAD
         [Authorize]
-
+=======
+>>>>>>> frontend-index&addtrip
         public IActionResult Index()
         {
             return View();
