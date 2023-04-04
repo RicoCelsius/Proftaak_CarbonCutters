d2448e76678e820549971deb4314234a4996c435
Rico <Rico.aarntzen@gmail.com>
2023-04-04 01:00:41 -0700

    removed comments

--- CarbonCutters/CarbonCutters/Controllers/ProfileController.cs
+++ CarbonCutters/CarbonCutters/Controllers/ProfileController.cs
@@ -12,24 +12,6 @@ namespace CarbonCuttersView.Controllers
             string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
             ViewBag.UserId = userId;
 
-
-
-            /*using (var db = new MyDbContext()
-            {
-                var userExists = db.Users.Any(u => u.UserId == userId);
-
-                if (userExists)
-                {
-                    // The user exists in the database
-                    // ..
-                }
-                else
-                {
-                    // The user does not exist in the database
-                    // ...
-                }
-            }*/
-
             return View();
         }
     }
